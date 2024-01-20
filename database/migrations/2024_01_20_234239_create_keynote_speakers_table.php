d1d3097e4b8ca7caa3236a57e20be6db66f417bc
Ahmed-Chaabane <137173423+Ahmed-Chaabane@users.noreply.github.com>
2024-01-21 00:44:08 +0100

    init commit

--- database/migrations/2024_01_20_234239_create_keynote_speakers_table.php
+++ database/migrations/2024_01_20_234239_create_keynote_speakers_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('countries', function (Blueprint $table) {
+        Schema::create('keynote_speakers', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('countries');
+        Schema::dropIfExists('keynote_speakers');
     }
 };
